cb5ae16224ca2b12518024a27f52c6a93abce372
hackmehust <ngohuythang1998@gmail.com>
2024-12-04 10:21:53 +0700

    first commit

---
+++ src/main/java/com/example/hackmehust/HackmehustApplication.java
@@ -0,0 +1,13 @@
+package com.example.hackmehust;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackmehustApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackmehustApplication.class, args);
+    }
+
+}
